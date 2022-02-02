c49eccc83ebe5465a16d927e64cc1822c57389a7
Karthik Kumar <karthik.krishnamoorthy@nagarro.com>
2022-02-03 01:04:48 +0530

    Listen updates and save

--- src/hacker-news/aggregator.js
+++ src/hacker-news/aggregator.js
@@ -6,6 +6,8 @@ const { filterJustStories } = require("./filters");
 const Setting = require("./models/setting");
 const Story = require("./models/story");
 
+let isFetchingNewStories = false;
+
 const getItemIdsToProcess = async (itemId) => {
   const latestItemId = parseInt(itemId);
   if (Number.isNaN(latestItemId)) return [];
@@ -20,35 +22,37 @@ const getItemIdsToProcess = async (itemId) => {
   return numbersInRange(lastProcessedItemId, latestItemId);
 };
 
-let isFetchingNewStories = false;
+const fetchAndUpdateStories = async (itemIds) => {
+  // HN Request: get item by id
+  const itemPromies = itemIds.map((itemId) =>
+    get(ref(firebaseDB, `v0/item/${itemId}`))
+  );
+  const itemSnapshots = await Promise.all(itemPromies);
+  const newStories = itemSnapshots.map((snapshot) =>
+    snapshot.exists() ? snapshot.val() : {}
+  );
+  const justStories = filterJustStories(newStories);
+
+  console.log({ justStories, count: justStories.length });
+
+  if (justStories.length) {
+    const saveStoryPromises = justStories.map((item) =>
+      Story.updateOne({ id: item.id }, { ...item }, { upsert: true })
+    );
+
+    // DB Request: Save the stories
+    await Promise.all(saveStoryPromises);
+    console.log(`saved ${saveStoryPromises.length} stories`);
+  }
+};
 
 const fetchNewStories = async (maxItemId) => {
   isFetchingNewStories = true;
   try {
     const itemIdsToProcess = await getItemIdsToProcess(maxItemId);
     console.log({ itemIdsToProcess });
-    // HN Request: get item by id
-    const itemPromies = itemIdsToProcess.map((itemId) =>
-      get(ref(firebaseDB, `v0/item/${itemId}`))
-    );
-    const itemSnapshots = await Promise.all(itemPromies);
-    const newStories = itemSnapshots.map((snapshot) =>
-      snapshot.exists() ? snapshot.val() : {}
-    );
-    const justStories = filterJustStories(newStories);
 
-    console.log({ justStories, count: justStories.length });
-
-    if (justStories.length) {
-      const saveStoryPromises = justStories.map((item) => {
-        const story = new Story({ ...item });
-        return story.save();
-      });
-
-      // DB Request: Save the stories
-      await Promise.all(saveStoryPromises);
-      console.log(`saved ${saveStoryPromises.length} stories`);
-    }
+    await fetchAndUpdateStories(itemIdsToProcess);
 
     // DB Request: Save the maxItemId as lastProcessedItemId
     await Setting.updateOne(
@@ -77,8 +81,21 @@ const fetchMaxItemAndNewStories = () => {
   });
 };
 
+const listenUpdates = () => {
+  // HN Request: get updates from /v0/updates
+  const updatesRef = ref(firebaseDB, "v0/updates");
+  onValue(updatesRef, (snapshot) => {
+    const updates = snapshot.val();
+    console.log(`Updates: ${updates.items?.length} items @ ${Date()}`);
+    if (updates.items?.length) {
+      fetchAndUpdateStories(updates.items);
+    }
+  });
+};
+
 const run = () => {
   fetchMaxItemAndNewStories();
+  listenUpdates();
 };
 
 module.exports = { run };
